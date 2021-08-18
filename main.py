54addcd762123beffd56586f37d0d778066221c5
ALDBanda <88377643+AlDBanda@users.noreply.github.com>
2021-08-18 15:19:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+#Tuple is round brackets
+#List is square brackets
+#Tuples is ordered
+#Tuple is immutable, cannot be changed
+#Tuple elements cannot be unique, same elements can be repeated
+#Tuple can contain different data types -integer, strings, text
+
+'''
+Creating a Tuple
+'''
+
+#tuple = ()
+#tuple = ('cat', [4,3,2,1], (1,2,3))
+#tuple = 1234, 4321
+#tuple = 'hello', #Always add coma otherwise its a string if its a single item for tuple use
+#print(tuple)
+
+
+
+'''
+Access Tuple Elements
+'''
+
+#tuple = (1234, 4321, 'hello')
+#print(tuple[-2])
+
+
+
+
+
+
+'''
+Slicing Tuples in Python
+'''
+#fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+#print(fruits[:])#the whole list
+#print(fruits[2:5])
+#print(fruits [:-2])
+#print(fruits[:2])
+#print(fruits[::2])
+#print(fruits[::-1])
+
+
+
+
+
+'''
+Changing a Tuple
+'''
+#fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+#fruits[0] = 'pear'
+
+
+
+'''
+Deleting a Tuple
+'''
+#fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+#del fruits[0]
+#print(fruits)
+#Not possible to change that eother
+
+#del fruits
+#print(fruits)
+
+#Not possible to change that either
+
+
+'''
+Tuple Methods
+'''
+
+#print(dir(tuple))
+
+#fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+
+#print(fruits.count('orange'))
+#print(fruits.index('apple'))
